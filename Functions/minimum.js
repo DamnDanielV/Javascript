f8bd45b311df35a6a43a6795126212d3a505f0f3
d4n137-coder <62660407+d4n137-coder@users.noreply.github.com>
2020-09-23 15:06:23 -0500

    function that finds the minimum between two numbers

---
+++ Functions/minimum.js
@@ -0,0 +1,8 @@
+'use strict'
+/**
+ * minimum : function two compares two numbers
+ * @param {int} num1 : first number to be compared
+ * @param {int} num2 : second number to be compared
+ * return: the minimum number
+ */
+const minimum = (num1, num2) => {return num1 < num2 ? num1 : num2;}
